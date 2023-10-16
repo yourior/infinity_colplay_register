0d88c25619adcbf6cf8e3d2d385ee04e1ee5eaee
yourior <37235534+yourior@users.noreply.github.com>
2023-10-16 19:12:10 +0700

    initial push

---
+++ index.js
@@ -0,0 +1,94 @@
+const puppeteer = require('puppeteer');
+var readlineSync = require('readline-sync');
+var fs = require('fs');
+
+var startTime, endTime;
+    function start() {
+        startTime = new Date();
+    };
+  
+  function end() {
+    endTime = new Date();
+    var timeDiff = endTime - startTime; //in ms
+    // strip the ms
+    timeDiff /= 1000;
+  
+    // get seconds 
+    var seconds = Math.round(timeDiff);
+    console.log("Time Wasted : "+seconds + " seconds");
+  }
+  async function Proxy(Location)
+    {
+        // Location = Location.trim();
+        // console.log(Location);
+        var Proxy = fs.readFileSync(Location, 'utf-8');
+    //   var Proxy = JSON.parse(Proxy);
+        i=0;
+        var Array = Proxy.split("\n")
+        console.log("Proxy Count : "+Array.length);
+        return Array;
+    }
+(async () => {
+
+    const proxy_count = Proxy("./inserproxy_here.txt")
+    //counting proxy
+    console.log("Proxy Count : "+ proxy_count.length);
+
+    try{
+        var TotalAttempt = readlineSync.question('How Many Email you want to register : ');
+        
+    }catch(err)
+    {
+        console.log("Error Log : "+err);
+        process.exit();
+    }
+    try{
+        var Domain = readlineSync.question('Domain Name(ex: gmail.com) : ');
+        
+    }catch(err)
+    {
+        console.log("Error Log : "+err);
+        process.exit();
+    }
+    try{
+        var Nation_ID = readlineSync.question('Country ID (ex: Indonesia = ID) : ');
+        
+    }catch(err)
+    {
+        console.log("Error Log : "+err);
+        process.exit();
+    }
+    //start process
+    start();
+    while(TotalAttempt && count<proxy_count.length)
+    {
+        console.log("Attempt Left : "+TotalAttempt+"Proxy Left : "+(proxy_count.length - count));
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+      
+        // Navigate to the website
+        await page.goto("https://infinity.coldplay.com");
+      
+        
+        // Generate random name and email
+        const randomName = `User${Math.floor(Math.random() * 10000)}`;
+        const randomEmail = randomName+"@"+Domain;
+      
+        // Fill out the form
+        await page.type("#name", randomName);
+        await page.type("#email", randomEmail);
+        await page.select("#country", Nation_ID); // Assuming ‘ID’ represents Indonesia
+      
+        // You can also submit the form if needed
+        // await page.click(‘#submit-button’);
+      
+        // Close the browser
+        await browser.close();
+        // process count
+        TotalAttempt--;
+        count++;
+    }
+    //end process
+    end();
+  
+})();
